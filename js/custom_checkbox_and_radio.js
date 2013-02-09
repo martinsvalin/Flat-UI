dce2325b81eefa80b5bb297aeb99fd6f4ba0fb5d
iurevych <iurevych@gmail.com>
2013-02-09 20:00:21 +0200

    Checkboxes and Radios

---
+++ js/custom_checkbox_and_radio.js
@@ -0,0 +1,46 @@
+// Custom checkbox and radios
+function setupLabel() {
+    // Checkbox
+    var checkBox = ".checkbox";
+    var checkBoxInput = checkBox + " input[type='checkbox']";
+    var checkBoxChecked = "checkbox-checked";
+    var checkBoxDisabled = "checkbox-disabled";
+
+    // Radio
+    var radio = ".radio";
+    var radioInput = radio + " input[type='radio']";
+    var radioOn = "radio-on";
+    var radioDisabled = "radio-disabled";
+
+    if ($(checkBoxInput).length) {
+        $(checkBox).each(function(){
+            $(this).removeClass(checkBoxChecked);
+        });
+        $(checkBoxInput + ":checked").each(function(){
+            $(this).parent(checkBox).addClass(checkBoxChecked);
+        });
+        $(checkBoxInput + ":disabled").each(function(){
+            $(this).parent(checkBox).addClass(checkBoxDisabled);
+        });
+    };
+    if ($(radioInput).length) {
+        $(radio).each(function(){
+            $(this).removeClass(radioOn);
+        });
+        $(radioInput + ":checked").each(function(){
+            $(this).parent(radio).addClass(radioOn);
+        });
+        $(radioInput + ":disabled").each(function(){
+            $(this).parent(radio).addClass(radioDisabled);
+        });
+    };
+};
+
+$(document).ready(function(){
+    $("body").addClass("has-js");
+    $(".checkbox, .radio").click(function(){
+        setupLabel();
+    });
+    setupLabel();
+});
+
