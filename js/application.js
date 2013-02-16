cc42ce8be693fafa8efa6915b668a0cb13ecb6a0
iurevych <iurevych@gmail.com>
2013-02-16 03:51:40 +0200

    Moving all JS inits into a separate file

---
+++ js/application.js
@@ -0,0 +1,24 @@
+// Some general UI pack related JS
+
+$(function () {
+    // Custom selects
+    $("select").dropkick();
+});
+
+$(document).ready(function() {
+    // Init tooltips
+    $("[data-toggle=tooltip]").tooltip("show");
+
+    // Init tags input
+    $("#tagsinput").tagsInput();
+
+    // Init jQuery UI slider
+    $("#slider").slider({
+        min: 1,
+        max: 5,
+        value: 2,
+        orientation: "horizontal",
+        range: "min",
+    });
+});
+
